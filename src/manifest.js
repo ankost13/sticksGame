51ca2bef662da750ee2740653da504f693830ded
Anastasiyka <anastasyam1908@gmail.com>
2025-02-26 23:44:57 +0200

    Add button in field

--- src/manifest.js
+++ src/manifest.js
@@ -5,6 +5,9 @@ export const manifest = {
             assets: {
                 bg: "assets/img/bg.jpg",
                 match: "assets/img/match.png",
+                button1: "assets/img/01.png",
+                button2: "assets/img/02.png",
+                button3: "assets/img/03.png",
             },
         },
     ]
--- src/modules/field/fieldView.js
+++ src/modules/field/fieldView.js
@@ -6,7 +6,9 @@ export class FieldView extends View {
     constructor(parent) {
         super(parent);
         this.collectionMatches = [];
+        this.collectionButton = [];
         this.addMatches();
+        this.addButton();
     }
 
     addMatches() {
@@ -25,6 +27,23 @@ export class FieldView extends View {
             match.y = this.startPositionY;
             this.parentForMatches.addChild(match);
             this.collectionMatches.push(match);
+        }
+    }
+
+    addButton() {
+        this.startPositionX = window.innerWidth / 2 - 400;
+        this.startPositionY = window.innerHeight / 2 + 230;
+
+        for (let i = 1; i <= this.numberMatches; i++) {
+            const button = new Sprite();
+            button.texture = Assets.get("button" + i);
+            button.anchor = .5;
+            button.scale = .3;
+            button.x = this.startPositionX + 200* i;
+            button.y = this.startPositionY;
+            button.interactive = true;
+            this.parentForMatches.addChild(button);
+            this.collectionButton.push(button);
         }
     }
 }
